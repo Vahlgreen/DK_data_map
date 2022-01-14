9dab33b716dd362c18ae9c3c62c8b3c4269a756d
Rasmus Vahlgreen <vahlgreen@live.dk>
2022-01-14 09:55:15 +0100

    Marks polygons with no data entries

--- Tvang_afslag_kort.py
+++ Tvang_afslag_kort.py
@@ -12,10 +12,10 @@ def truncate_colormap(cmap, minval=0.3, maxval=1.0, n=100):
 
 
 df = gpd.read_file("KOM_MULTIPART.shp")
-data = pd.read_excel("Regression af hver enkelt kommune.xlsx")
+data = pd.read_excel("Regression_af_hver_enkelt_kommune.xlsx")
 df = df.drop(54, axis = 0).reset_index()
-df = df.sort_values(by="KOMNAVN")
-data = data.sort_values(by="Kommune")
+#df = df.sort_values(by="KOMNAVN")
+#data = data.sort_values(by="Kommune")
 df["KOMNAVN"] = df["KOMNAVN"].replace("Aarhus","Århus")
 #remove christianssø
 
@@ -33,12 +33,27 @@ df["AFSLAG"] = pd.DataFrame(data_to_add)
 
 
 
+df["AFSLAG"].replace(np.NaN,48.46,inplace=True)
+df["AFSLAG"].replace(-6.82000,np.NaN,inplace=True)
+df["AFSLAG"].replace(-2.88000,np.NaN,inplace=True)
+df["AFSLAG"].replace(-18.32000,np.NaN,inplace=True)
+df["AFSLAG"].replace(-3.60000,np.NaN,inplace=True)
+
+
 #plot map
+#"{%.2%%}"
 cmap = truncate_colormap(cmap = plt.get_cmap("Reds"))
-df.to_crs(epsg=4326).plot(column="AFSLAG", cmap = cmap, legend = True)
-plt.title("Afslag i pris på tvangsauktion")
+df.to_crs(epsg=4326).plot(column="AFSLAG", cmap = cmap, legend = True,
+        missing_kwds={
+        "color": "lightgrey",
+        "edgecolor": "blue",
+        "hatch": "///",
+        "label": "Missing values",
+    })
+plt.title("Procentvis afslag i pris på tvangsauktion")
 plt.xlabel("Længdegrad")
 plt.ylabel("Breddegrad")
+
 
 plt.show()
 
--- test.py
+++ test.py
@@ -20,33 +20,46 @@ df["KOMNAVN"] = df["KOMNAVN"].replace("Aarhus","Århus")
 #remove christianssø
 
 
-data_neg = data[data["Afslag"]<0]
-data_pos = data[data["Afslag"]>0]
-df_pos=pd.DataFrame()
-df_neg = pd.DataFrame(columns=df.columns)
-for index, row in df.iterrows():
-    for i in range(df.shape[0]):
-        df_neg.append(row)
+data_neg = data[data["Afslag"]<0].reset_index()
+data_pos = data[data["Afslag"]>0].reset_index()
+
+polygons = []
+neg_rows = [2,0,1,3]
+for i in neg_rows:
+    polygons.append(df.iloc[i]["geometry"])
+data_neg["geometry"]=polygons
+
+
+polygons = []
+pos_rows = [num for num in range(4,df.shape[0])]
+
+for i in range(4,df.shape[0]):
+    polygons.append(df.iloc[i]["geometry"])
+data_pos["geometry"] = polygons
 
 
 
-tmp=[]
-for index,row in data.iterrows():
-    tmp.append((row["Kommune"],row["Afslag"]))
 
-tmp = dict(tmp)
-data_to_add=[]
-for name in df["KOMNAVN"]:
-    for key in tmp:
-        if (name==key):
-            data_to_add.append(tmp[key])
-df["AFSLAG"] = pd.DataFrame(data_to_add)
+#tmp=[]
+#for index,row in data.iterrows():
+#    tmp.append((row["Kommune"],row["Afslag"]))
+#
+#tmp = dict(tmp)
+#data_to_add=[]
+#for name in df["KOMNAVN"]:
+#    for key in tmp:
+#        if (name==key):
+#            data_to_add.append(tmp[key])
+#df["AFSLAG"] = pd.DataFrame(data_to_add)
 
 
 
 #plot map
 cmap = truncate_colormap(cmap = plt.get_cmap("Reds"))
-df.to_crs(epsg=4326).plot(column="AFSLAG", cmap = cmap, legend = True)
+df_pos = gpd.GeoDataFrame(data = data_pos, geometry="geometry",crs="EPSG:4326")
+df_neg = gpd.GeoDataFrame(data = data_neg, geometry="geometry",crs="EPSG:4326")
+df_pos.to_crs(epsg=4326).plot(column="Afslag", cmap = cmap, legend = True)
+df_neg.to_crs(epsg=4326).plot(column="Afslag", cmap = cmap, legend = True)
 plt.title("Afslag i pris på tvangsauktion")
 plt.xlabel("Længdegrad")
 plt.ylabel("Breddegrad")
